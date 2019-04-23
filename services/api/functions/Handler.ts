e9d135a20b48787f6960dd9e44e1d7edcf5c5373
jack <jack.holmes@vacasa.com>
2019-04-23 19:33:57 +1200

    Use aws-lambda types

--- services/api/functions/Handler.ts
+++ services/api/functions/Handler.ts
@@ -1,7 +1,6 @@
-import IEvent from '../lib/IEvent'
-import IResponse from '../lib/IResponse'
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const index = async (event: IEvent): Promise<IResponse> => {
+export const index = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
     return {
         statusCode: 200,
         body: 'This is a lambda function'
