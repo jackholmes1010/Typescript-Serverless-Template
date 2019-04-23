6d895d7a9c4055ab086fb9cd945b350c54519ec6
jack <jack.holmes@vacasa.com>
2019-04-23 19:35:28 +1200

    Remove cached

--- services/api/build/functions/Handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = async (event) => {
-    return {
-        statusCode: 200,
-        body: 'This is a lambda function'
-    };
-};
--- services/api/build/lib/IResponse.d.ts
+++
@@ -1,5 +0,0 @@
-export default interface IResponse {
-    statusCode: number;
-    body?: string | undefined;
-    headers?: object | undefined;
-}
